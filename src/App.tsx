d20128697b7fb6267f47d09d915a2c06c14657d3
Matias Luengo <matiasluengoo95@gmail.com>
2024-09-05 12:25:02 -0300

    install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ListOfUsers from "./components/ListOfUsers";
+function App() {
+  return (
+    <main className="w-[95%] mx-auto">
+      <h1>Redux</h1>
+      <ListOfUsers />
+    </main>
+  );
+}
+
+export default App;
